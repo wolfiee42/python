d1b809b14cb232637b6bd133039bbb9a734dcd30
wolfiee42 <saifalislam2022@gmail.com>
2025-11-17 07:32:56 +0600

    learn: learning type casting in python.

---
+++ module-1/type_casting.py
@@ -0,0 +1,18 @@
+# Type casting
+
+name = "Saif"
+age = 24
+gpa = 3.12
+is_student = False
+
+# print(type(is_student))
+
+# age = float(age)
+age = str(age)
+# print(type(age))
+
+gpa = int(gpa)
+# print(gpa)
+
+name = bool(name)
+print(name)
