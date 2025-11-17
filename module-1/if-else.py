fefed0d21cd4a01cc299337bfa56756d7fb7978b
wolfiee42 <saifalislam2022@gmail.com>
2025-11-17 09:06:38 +0600

    learn: understanding if else syntax in python

---
+++ module-1/if-else.py
@@ -0,0 +1,30 @@
+#  if else statement
+
+
+
+
+
+
+
+# response = input("Do you like food? (Y/N): ")
+
+# if response == "Y":
+#     print("Here is some food for you")
+# else:
+#     print("No food for you")
+
+
+
+
+
+# age = int(input("enter your age: "))
+
+# if  age >= 100:
+#     print("You are too old to sign up")
+# elif age >=18:
+#     print("You are signed up")
+# elif age <=0:
+#     print("you haven't born yet!")
+# else:
+#     print("you must be 18+ to sign up")
+
