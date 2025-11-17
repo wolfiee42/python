b3545c4bdb21df6e307178a3ed02ca7cdd239fba
wolfiee42 <saifalislam2022@gmail.com>
2025-11-17 08:24:19 +0600

    learn: creating a madlibs game in python

---
+++ module-1/madlibs.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter s verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
