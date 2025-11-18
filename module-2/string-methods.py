72f301b1666e490f90b7b16666aa0f0d5d81673e
wolfiee42 <saifalislam2022@gmail.com>
2025-11-18 09:01:09 +0600

    learn: learning string-method in python

---
+++ module-2/string-methods.py
@@ -0,0 +1,18 @@
+# name = input("Type your name: ")
+# phone_number = input("Enter your phone number: ")
+
+# result = len(name)
+# result = name.rfind("a")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_number.count("-")
+# phone_number = phone_number.replace("-", " ")
+# phone_number = phone_number.replace("-", "")
+
+# print(phone_number)
+
+# all string methods 
+print(help(str))
