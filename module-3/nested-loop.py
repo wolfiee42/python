a454fb9384df318b67bfafb74565003acd9ba9f1
wolfiee42 <saifalislam2022@gmail.com>
2025-11-20 08:57:50 +0600

    learn: learning nested loop in python

---
+++ module-3/nested-loop.py
@@ -0,0 +1,18 @@
+# 2
+rows = int(input("Enter the # of rows: "))
+col = int(input("Enter the # of colums: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(col):
+        print(symbol, end="")
+    print()
+
+
+
+
+# 1
+# for x in range(3):
+#     for y in range(10):
+#         print(y, end="")
+#     print()
