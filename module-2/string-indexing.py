76b88ddaa7a408567f514d8bcd44904165680da4
wolfiee42 <saifalislam2022@gmail.com>
2025-11-20 07:37:29 +0600

    learn: learning string indexing in python

---
+++ module-2/string-indexing.py
@@ -0,0 +1,16 @@
+# index in python
+        # [start : end : step]
+
+credit_number = "1234-5678-9012-2345"
+
+# last_digits = credit_number[-4:]
+
+credit_number = credit_number[::-1]
+
+# print(credit_number[0:4])
+# print(credit_number[:4])
+# print(credit_number[5:])
+# print(credit_number[-1]) # reverse index
+# print(credit_number[::3])  # steps
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+print(credit_number)
