c9acea2d45e953029fe79f10ea24e87bbcb8cbfd
wolfiee42 <saifalislam2022@gmail.com>
2025-11-20 07:46:44 +0600

    learn: learning format spcifier in python

---
+++ module-2/format-specifier.py
@@ -0,0 +1,15 @@
+price1 = 34536.453
+price2 = 23235234.32
+price3 = -3123214.21
+
+# print(f"Price 1 is ${price1:<10}")
+# print(f"Price 2 is ${price2:<10}")
+# print(f"Price 3 is ${price3:<10}")
+
+# print(f"Price 1 is ${price1:+}")
+# print(f"Price 2 is ${price2:+}")
+# print(f"Price 3 is ${price3:+}")
+
+print(f"Price 1 is ${price1:,.2f}")
+print(f"Price 2 is ${price2:,.2f}")
+print(f"Price 3 is ${price3:,.2f}")
