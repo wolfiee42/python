8589af3ff0014e35b8e1f1008688496d5dce356d
wolfiee42 <saifalislam2022@gmail.com>
2025-11-20 08:03:55 +0600

    learn: learning while loop in python

---
+++ module-3/while-loop.py
@@ -0,0 +1,27 @@
+# 3
+num = int(input("Enter a num between 1 - 10: "))
+
+while num < 0 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a num between 1 - 10: "))
+
+print(f"your num is {num}")
+
+# 2
+# food = input("Enter a food you like (q to quit): ")
+# 
+# while not food == "q":
+#     print(f"you like {food}")
+#     food = input("Enter another food you like (q to quit): ")
+# 
+# print("bye")
+
+# 1
+# name = input("Enter your name here: ")
+# 
+# while name == "":
+#     print("You did not enter your name.")
+#     name = input("Enter your name here: ")
+# 
+# print(f"Hello, {name}")
+
