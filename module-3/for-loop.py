b2d5f43931f41b7f28ca13533cd6425f22c70802
wolfiee42 <saifalislam2022@gmail.com>
2025-11-20 08:38:54 +0600

    learn: learning for loops in python

---
+++ module-3/for-loop.py
@@ -0,0 +1,17 @@
+# 3
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    print(x)
+
+# 2
+# for x in range(1, 11, 2): # in steps
+#    print(x)
+
+
+
+# 1
+# for x in reversed(range(1, 11)):
+#     print(x)
+# 
+# print("Happy new year") 
