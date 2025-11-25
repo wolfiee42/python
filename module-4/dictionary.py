b15bf87997788a1a3b6af4b5fe59586c901683e3
wolfiee42 <saifalislam2022@gmail.com>
2025-11-25 06:40:52 +0600

    learn: learning dictionary in python

---
+++ module-4/dictionary.py
@@ -0,0 +1,29 @@
+capitals = {
+    "BD": "Dhaka",
+    "IND": "New Delhi",
+    "KSA": "Riyadh",
+    "UAE": "Abu Dhabi"
+}
+
+
+# print(capitals.get("PK"))
+
+# if capitals.get("PK"):
+    # print("That capital exist")
+# else:
+    # print("That capital does not exist")
+
+# capitals.update({"GER": "Berlin"})
+# capitals.update({"BD": "Chattogram"})
+# capitals.pop("IND")
+# capitals.popitem()
+# capitals.clear()
+
+
+keys = capitals.keys()
+
+values = capitals.values()
+
+items = capitals.items()
+
+print(items)
