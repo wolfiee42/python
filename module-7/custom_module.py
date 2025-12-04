38c351f89c7aff3cc9588d876431c81a6503af2f
wolfiee42 <saifalislam2022@gmail.com>
2025-12-04 09:24:42 +0600

    learn: learning how to create module in python

---
+++ module-7/custom_module.py
@@ -0,0 +1,7 @@
+pi = 3.1415
+
+def square(x):
+    return x*2
+
+def cube(x):
+    return x*3
---
+++ module-7/module.py
@@ -0,0 +1,13 @@
+# import math
+# import math as m
+# from math import pi
+from custom_module import square, cube
+
+
+
+# print(math.pi)
+# print(m.pi)
+# print(pi)
+# print(square(5))
+# print(cube(5))
+print(help("modules"))
