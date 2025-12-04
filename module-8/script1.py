f62aa484c9bb233179946b68e0400cc2f90b0e0b
wolfiee42 <saifalislam2022@gmail.com>
2025-12-04 09:46:55 +0600

    learn: learning the fundamental of __name__ nad __main__

---
+++ module-8/script1.py
@@ -0,0 +1,8 @@
+def favorite_food(food):
+    print(f"your favourite food is {food}")
+
+def main():
+    print("Script 001")
+
+if __name__ == "__main__":
+    main()
---
+++ module-8/script2.py
@@ -0,0 +1,12 @@
+from script1 import *
+
+def favorite_drink(drink):
+    print(f"your favourite drink is {drink}")
+
+def main():
+    print("Script002")
+    favorite_drink("coffee")
+    favorite_food("egg")
+    
+if __name__ == "__main__":
+    main()
