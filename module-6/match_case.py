dbb18be08c5e6a3bd7cd5e12af6f61f1fab4e4ee
wolfiee42 <saifalislam2022@gmail.com>
2025-12-04 09:15:02 +0600

    learn: learning match case in python

--- module-6/match_case.py
+++ module-6/match_case.py
@@ -1,17 +1,31 @@
 def days_of_week(day):
-    if day == 1:
-        return "It is Monday"
-    if day == 2:
-        return "It is Tuesday"
-    if day == 3:
-        return "It is Wednesday"
-    if day == 4:
-        return "It is Thursday"
-    if day == 5:
-        return "It is Friday"
-    if day == 6:
-        return "It is Saturday"
-    if day == 7:
-        return "It is Sunday"
+    match day:
+        case 1:
+            return "It is Monday"
+        case 2:
+            return "It is Tuesday"
+        case 3:
+            return "It is Wednesday"
+        case 4:
+            return "It is Thursday"
+        case 5:
+            return "It is Friday"
+        case 6:
+            return "It is Saturday"
+        case 7:
+            return "It is Sunday"
+        case _:
+            return "It is an invalid day."
     
-print(days_of_week(1))
+# print(days_of_week(1))
+
+def is_weekend(day):
+    match day:
+        case "Saturday" | "Sunday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _:
+            return False
+
+print(is_weekend("Wednesday"))
