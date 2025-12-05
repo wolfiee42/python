84987e349415e0d9219657b9c3aed91af2afbdee
wolfiee42 <saifalislam2022@gmail.com>
2025-12-05 20:31:19 +0600

    feat: banking program written in python

--- module-8/bank.py
+++ module-8/bank.py
@@ -1,39 +1,61 @@
 #  exercise
 
-def show_balance():
-    pass
+def show_balance(balance):
+    print(f"Your balance is {balance:.2f}")
 
 def deposit():
-    pass
+    amount = float(input("Enter your amount: "))
 
-def withdraw():
-    pass
+    if amount < 0:
+        print(f"{amount} is not a valid amount to deposit")
+        return 0
+    else:
+        return amount
 
-balance = 0
-is_running = True
+def withdraw(balance):
+    amount = float(input("Enter your amount: "))
 
-while is_running:
-    print("Banking Program")
-    print("1. Show Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-    
-    choice = input("Enter your choice (1-4): ")
-    
-    if choice == '1':
-        show_balance()
-    
-    elif choice == '2':
-        deposit()
+    if amount > balance:
+        print("Insufficiate Funds")
+        return 0
+    elif amount < 0:
+        print(f"{amount} should be greater than 0")
+        return 0
+    else:
+        return amount
+
+
+
+def main():
+
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("Banking Program")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
         
-    elif choice == '3':
-        withdraw()
+        choice = input("Enter your choice (1-4): ")
         
-    elif choice == '4':
-        is_running = False
+        if choice == '1':
+            show_balance(balance)
         
-    else: 
-        print("This is an invalid input.")
-        
-print("Thank you! Have a nice day!")
+        elif choice == '2':
+            balance += deposit()
+            
+        elif choice == '3':
+            balance -= withdraw(balance)
+            
+        elif choice == '4':
+            is_running = False
+            
+        else: 
+            print("This is an invalid input.")
+            
+    print("Thank you! Have a nice day!")
+
+if __name__ == '__main__':
+    main()
