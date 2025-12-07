f7bd27a9c7e6afeb5dea77d2d3e22933e36209da
wolfiee42 <saifalislam2022@gmail.com>
2025-12-07 07:35:23 +0600

    learn: learning multiple and multilevel inheritence in python

---
+++ module-9/multiple_inheritence.py
@@ -0,0 +1,34 @@
+class Animal:
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+
+rabbit = Rabbit("tony")
+hawk = Hawk("stark")
+fish = Fish("nemo")
+
+fish.sleep()
